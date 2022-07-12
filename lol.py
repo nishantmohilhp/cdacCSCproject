27efb4991405c59f0ff821b983068d50b1138102
nishantmohilhp <108318455+nishantmohilhp@users.noreply.github.com>
2022-07-12 23:11:30 +0530

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,7 +1,10 @@
 from scapy.all import sniff
 
 def packet_callback(packet): 
-    print(packet.show())
+    mypacket = str(packet[TCP].payload)
+    if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
+        print(f"[*] Destination: {packet[IP].dst}")
+        print(f"[*] {str(packet[TCP].payload)}")
 
 def main():
     sniff(prn=packet_callback, count=1)
